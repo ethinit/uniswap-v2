4aaf55e5ba5ac5240fe98d51fb7337c7b8f1ba8c
Stefan Eroteev <eroteev@gmail.com>
2021-08-26 22:26:28 +0300

    Create a demo using Mocha and Chai

---
+++ tests/dex.ts
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+import { Dex } from '../src/dex';
+
+describe('Prices', () => {
+  it("should return price", () => {
+    // let dex = new Dex();;
+    expect(true).to.equal(true);
+  });
+});
