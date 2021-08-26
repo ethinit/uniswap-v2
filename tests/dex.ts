0ac3830977c8c24877f16a0725091042d797debd
Stefan Eroteev <eroteev@gmail.com>
2021-08-26 22:35:33 +0300

    Cleanup

--- tests/dex.ts
+++ tests/dex.ts
@@ -3,7 +3,7 @@ import { Dex } from '../src/dex';
 
 describe('Prices', () => {
   it("should return price", () => {
-    // let dex = new Dex();;
+    // let dex = new Dex();
     expect(true).to.equal(true);
   });
 });
